e0f1c4224ecc60b2b8b1f02431c19b06d85fe17e
akshaykanive <akshaykanive@gmail.com>
2023-02-21 00:03:50 +0530

    Modified code for Holi Beta Testing

--- whatsapp_merge_image.py
+++ whatsapp_merge_image.py
@@ -10,13 +10,33 @@ In case of any queries, please write to akshaykanive@gmail.com
 import pywhatkit
 
 #Get the phone numbers. Not sure what happends if non-whatsapp number is given.
-phone_nos=["+919611261259"]
+phone_nos=["+917892209752","+919725400338","+918970935199","+916203953049","+919817526747","+918884781268","+918050529348","+919606333566","+918296137566","+917204489655","+918861004710","+918277208895","+919831505081","+917204961211"]
+print("Send the message for ",len(phone_nos), "people")
 #Get the names of the participipants
-names=["Akshay"]
+names=["Prasanna"]
 #Get the payment reference for each participants
-payment_ref=["dummy_payment_id"]
-path="/Users/akshaykanive/Downloads/rotacamp.png"
+#payment_ref=["dummy_payment_id"]
+path="/Users/akshaykanive/Desktop/holi_poster.jpeg"
+
+message="""
+Warm Rotaract Greetings,
+
+Hope you're doing well. 
+
+We, the Rotaract club of Bangalore Jayanagar and Rotaract Bangalore West are bringing to you, the biggest and the first-ever Holi Celebration Event of RI District 3190 happening on 04 March 2023.  
+
+The Individual registrations are now open at rotaractjayanagar.org
+
+We are looking forward for your collaboration, the collaboration criteria is as follows:
+
+📌 *Collaboration Criteria* - 15 registrations for institution-based clubs and 10 registrations for community-based clubs.
+
+We have special offers for bulk registrations. 
+Looking forward to hosting your club, come join us to celebrate Holi not only to spread joy but also to serve a cause.
+
+Please get in touch with regards to the registrations.
+"""
 
 for i in range(len(phone_nos)):
-    message="Hello "+names[i]+",\nThank you for registerting for Rotaract Dist Conference.\n\n*Payment Reference:* "+payment_ref[i]+"\nPlease find the attached pass for the same. \nThis is an automated message. Please do not reply.😂 \n"
-    pywhatkit.sendwhats_image(phone_nos[i],path,message,tab_close=True)
+    #message="Hello "+names[i]+",\nThank you for registerting for Rotaract Dist Conference.\n\n*Payment Reference:* "+payment_ref[i]+"\nPlease find the attached pass for the same. \nThis is an automated message. Please do not reply.😂 \n"
+    pywhatkit.sendwhats_image(phone_nos[i],path,message,tab_close=True,close_time=8)
