5ebbb86e6352bea417b1dd556e41a2ea21938863
akshaykanive <akshaykanive@gmail.com>
2023-03-04 03:16:19 +0530

    Added csv parsing logic for whatsapp merge with image code

--- whatsapp_merge_image.py
+++ whatsapp_merge_image.py
@@ -9,34 +9,32 @@ In case of any queries, please write to akshaykanive@gmail.com
 #Import required modules
 import pywhatkit
 
-#Get the phone numbers. Not sure what happends if non-whatsapp number is given.
-phone_nos=["+917892209752","+919725400338","+918970935199","+916203953049","+919817526747","+918884781268","+918050529348","+919606333566","+918296137566","+917204489655","+918861004710","+918277208895","+919831505081","+917204961211"]
-print("Send the message for ",len(phone_nos), "people")
-#Get the names of the participipants
-names=["Prasanna"]
-#Get the payment reference for each participants
-#payment_ref=["dummy_payment_id"]
-path="/Users/akshaykanive/Desktop/holi_poster.jpeg"
-
 message="""
-Warm Rotaract Greetings,
+Warm Rotaract Greetings!
 
-Hope you're doing well. 
+Thank you for registering for HoliYay!
+Here is your pass for the event. Please ensure to check the rules and regulations for the event.
+https://bit.ly/holiyay2023
 
-We, the Rotaract club of Bangalore Jayanagar and Rotaract Bangalore West are bringing to you, the biggest and the first-ever Holi Celebration Event of RI District 3190 happening on 04 March 2023.  
-
-The Individual registrations are now open at rotaractjayanagar.org
-
-We are looking forward for your collaboration, the collaboration criteria is as follows:
-
-📌 *Collaboration Criteria* - 15 registrations for institution-based clubs and 10 registrations for community-based clubs.
-
-We have special offers for bulk registrations. 
-Looking forward to hosting your club, come join us to celebrate Holi not only to spread joy but also to serve a cause.
-
-Please get in touch with regards to the registrations.
+Awaiting to see you there!
 """
 
-for i in range(len(phone_nos)):
-    #message="Hello "+names[i]+",\nThank you for registerting for Rotaract Dist Conference.\n\n*Payment Reference:* "+payment_ref[i]+"\nPlease find the attached pass for the same. \nThis is an automated message. Please do not reply.😂 \n"
-    pywhatkit.sendwhats_image(phone_nos[i],path,message,tab_close=True,close_time=8)
+#Read the registration file
+reg_file = open("holi_2.csv", "r")
+
+for registration in reg_file:
+    registration=registration.strip()
+    reg_info=registration.split(",")
+    
+    #Get the phone numbers. Not sure what happends if non-whatsapp number is given.
+    phone_no=reg_info[3] 
+    #phone_no="+919060375749"
+
+    #Get the names of the participipants
+    names=reg_info[1]
+
+    #Get the payment reference for each participants
+    payment_ref=reg_info[2]
+    path="./Pass/"+payment_ref+".png"
+    
+    pywhatkit.sendwhats_image(phone_no,path,message,tab_close=True,close_time=8)
